16e63c5d7c66c65f3c88c07eeeac82092434d1b4
Mahin Jasim <barakattanzil@gmail.comm>
2023-06-08 03:04:06 +0600

    user saved done

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 
+const corsOptions = {
+    origin: '*',
+    credentials: true,
+    optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
+app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ngcnpjb.mongodb.net/?retryWrites=true&w=majority`;
@@ -23,16 +30,27 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         await client.connect();
-       
 
+        const usersCollection = client.db('danceDB').collection('users');
 
-
+// all users...........start.....
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email };
+            const existingUser = await usersCollection.findOne(query);
+            if (existingUser) {
+                return res.send({ message: 'user already exist' })
+            }
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
+// all users.......end.....
 
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
